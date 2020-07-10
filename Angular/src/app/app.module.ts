630e55e7f21b9e7f99790daf91fb1e5c2647dd3a
Praveen <rgp.praveen@gmail.com>
2020-07-10 08:39:21 +0530

    Initial Commit
    Project creation

---
+++ Angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MaterialModule } from './shared/material.module';
+import { MaterialCardComponent } from './components/material-card/material-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MaterialCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
